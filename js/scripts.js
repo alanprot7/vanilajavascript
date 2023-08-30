9e33a7c1643829a5193498c3b55e7d3854d64dc2
Alan Oliveira <alanprot7@gmail.com>
2023-08-30 11:04:57 -0300

    Checando repetidos

--- js/scripts.js
+++ js/scripts.js
@@ -13,38 +13,42 @@ let oldInputValue;
 
 // Funções
 const saveTodo = (text, done = 0, save = 1) => {
-  const todo = document.createElement("div");
-  todo.classList.add("todo");
+  
+  if(checkUniqueTodo(text.trim())) {
 
-  const todoTitle = document.createElement("h3");
-  todoTitle.innerText = text;
-  todo.appendChild(todoTitle);
-
-  const doneBtn = document.createElement("button");
-  doneBtn.classList.add("finish-todo");
-  doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
-  todo.appendChild(doneBtn);
-
-  const editBtn = document.createElement("button");
-  editBtn.classList.add("edit-todo");
-  editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
-  todo.appendChild(editBtn);
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("remove-todo");
-  deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-  todo.appendChild(deleteBtn);
-
-  // Utilizando dados da localStorage
-  if (done) {
-    todo.classList.add("done");
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+  
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text.trim();
+    todo.appendChild(todoTitle);
+  
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+  
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn);
+  
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    todo.appendChild(deleteBtn);
+  
+    // Utilizando dados da localStorage
+    if (done) {
+      todo.classList.add("done");
+    }
+  
+    if (save) {
+      saveTodoLocalStorage({ text, done: 0 });
+    }
+  
+    todoList.appendChild(todo);
   }
-
-  if (save) {
-    saveTodoLocalStorage({ text, done: 0 });
-  }
-
-  todoList.appendChild(todo);
 
   todoInput.value = "";
 };
@@ -85,6 +89,20 @@ const getSearchedTodos = (search) => {
     }
   });
 };
+
+const checkUniqueTodo = (text) => {
+  const todos = document.querySelectorAll(".todo");
+  let unique = true
+
+  todos.forEach((todo) => {
+    const todoTitle = todo.querySelector("h3").innerText.toLowerCase()
+
+    if (todoTitle == text.toLowerCase()) {
+        unique = false
+    }
+  })
+  return unique
+}
 
 const filterTodos = (filterValue) => {
   const todos = document.querySelectorAll(".todo");
